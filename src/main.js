fb22efb72328edbdc362adab5eaece3a1a284aa5
Aditya Oberai <adityaoberai1@gmail.com>
2024-07-27 07:35:48 +0000

    Fix return response

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ export default async ({ req, res, log, error }) => {
     let generation = result.response.text();
     log(generation);
 
-    res.json({
+    return res.json({
       result: generation
     });
   }
